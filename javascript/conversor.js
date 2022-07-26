e28334a0f44008470f369f5a6bf47668a5c88062
PETRUS <88741218+PetrusNoleto@users.noreply.github.com>
2022-07-26 11:37:27 -0300

    update

--- javascript/conversor.js
+++ javascript/conversor.js
@@ -4,20 +4,20 @@
     function converter(){
         const _fontSizeDefault = 16
         let $fontSize = document.querySelector(".getfontsize").value
-        let $pxvalue = document.querySelector(".getPixels").value
+        let $remvalue = document.querySelector(".getRem").value
     
             if($fontSize == _fontSizeDefault){
-                let _calcFontSizeDefault = Number($pxvalue / _fontSizeDefault) 
+                let _calcFontSizeDefault = Number(_fontSizeDefault * $remvalue) 
                 let _result = _calcFontSizeDefault
                 document.querySelector(".resulttxt").textContent = _result
 
             }
             else($fontSize != _fontSizeDefault) 
-                let _remcalc = Number($pxvalue / $fontSize)
+                let _remcalc = Number($fontSize * $remvalue )
                 document.querySelector(".resulttxt").textContent = (_remcalc)
         
             
-            if($fontSize, $pxvalue == 0){
+            if($fontSize, $remvalue == 0){
                 document.querySelector(".resulttxt").textContent = ("")
             }
         
